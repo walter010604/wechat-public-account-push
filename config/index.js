4e2fd81a65bb1c46900daa9ac94931a5ab4944b0
walter010604 <112762572+walter010604@users.noreply.github.com>
2022-09-03 21:06:41 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "	PMj5N7pQh9pf4AEOB-MupHrmdKuh_HyojFHuN4U9ceY",
+  CALLBACK_TEMPLATE_ID: "PMj5N7pQh9pf4AEOB-MupHrmdKuh_HyojFHuN4U9ceY",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
